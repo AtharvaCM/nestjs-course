13ba8210615886b65a7ea2dd0ab89af8b058aef4
AtharvaCM <necrons999@gmail.com>
2024-04-10 15:04:57 +0530

    [8-48] A Few Extra Routes

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [Report, User],
+      synchronize: true, // Don't use this in production
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
