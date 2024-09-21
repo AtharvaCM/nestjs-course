e87367e03e2c54ced4e32f4ac5b242eed706eb97
AtharvaCM <necrons999@gmail.com>
2024-09-21 11:34:41 +0530

    [13-117] Creating Separate Test and Dev Databases

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -1,18 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cookieSession = require('cookie-session');
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['sjabdasd'],
-    }),
-  );
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ mycv/test/auth.e2e-spec.ts
@@ -0,0 +1,36 @@
+import * as request from 'supertest';
+
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AppModule } from '../src/app.module';
+
+describe('Authentication System (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const email = 'testemail1@example.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: 'mypassword',
+      })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});
