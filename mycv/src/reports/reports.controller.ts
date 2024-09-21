1e6c5cf5177728cdab74c67f92c8684c2f39f39c
AtharvaCM <necrons999@gmail.com>
2024-09-21 15:30:50 +0530

    [15-135] Setting up the Association

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { User } from 'src/users/user.entity';
+
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { CreateReportDto } from './dtos/create-report-dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+}
